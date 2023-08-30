4c94d7e35b99dc5b1a4ecfae265d33be471030af
kratos <desmond8500@gmail.com>
2023-08-30 03:49:28 +0000

    update

---
+++ app/Http/Controllers/CDNController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class CDNController extends Controller
+{
+    function kit() : string {
+        $css = "
+            .text-primary{
+                color: blue;
+            }
+        ";
+
+        $response = $css;
+        // $response = Response::make($css);
+        // $response->header('Content-Type', 'text/css');
+
+        return $response;
+    }
+}
